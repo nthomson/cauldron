ab33db2d8ab149b8f88e59b1465904de44504e59
Nicholas Thomson <nthomsondev@gmail.com>
2019-08-05 01:26:21 -0400

    Nav visuals, initial home page

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Scene, Item, Page, StatBlock, Table } from '../';
 
 import './editor.scss';
 
-const Editor = () => {
-    return <div className='container'>
-        Hello, Editor!
+const Editor = () => (
+    <div className='container'>
+        <Page title='A Homebrew Adventure'>
+            <div className='col50'>
+                <h2>Nullam nec gravida sapien</h2>
+                <p>Proin a ligula scelerisque, tempus lacus sodales, sagittis justo. Etiam a laoreet quam, dictum volutpat quam. Donec sodales eu libero in dignissim. Fusce venenatis neque sit amet commodo auctor. </p>
+                <Scene>Donec in nulla non diam semper tempor. Nullam nec gravida sapien. Morbi volutpat condimentum nibh vitae luctus. Nulla vulputate turpis mauris, eu tempus dolor accumsan sed.</Scene>
+                <Table />
+                <Item name='Staff of Focused Pain' type='Staff, Arcane Focus'>
+                    +1 Spell Attack, +1 Spell Saving Throw DC <br />
+                    <strong>Action:</strong> You are able to use the crystal embedded into this staff in order to focus
+                    your pain, recovering one expended spell slot. You are only able to do this once per day.
+                </Item>
+            </div>
+            <div className='col50'>
+                <StatBlock monsterId={71} />
+            </div>
+        </Page>
     </div>
-};
+);
 
 export default Editor;
 
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,25 +1,13 @@
-import React, { Fragment } from 'react';
-import { Scene, Item, Page, StatBlock, Table } from '../';
+import React from 'react';
+import './home.scss';
 
 const Home = () => (
-    <Fragment>
-        <Page title='A Homebrew Adventure'>
-            <div className='col50'>
-                <h2>Nullam nec gravida sapien</h2>
-                <p>Proin a ligula scelerisque, tempus lacus sodales, sagittis justo. Etiam a laoreet quam, dictum volutpat quam. Donec sodales eu libero in dignissim. Fusce venenatis neque sit amet commodo auctor. </p>
-                <Scene>Donec in nulla non diam semper tempor. Nullam nec gravida sapien. Morbi volutpat condimentum nibh vitae luctus. Nulla vulputate turpis mauris, eu tempus dolor accumsan sed.</Scene>
-                <Table />
-                <Item name='Staff of Focused Pain' type='Staff, Arcane Focus'>
-                    +1 Spell Attack, +1 Spell Saving Throw DC <br />
-                    <strong>Action:</strong> You are able to use the crystal embedded into this staff in order to focus
-                    your pain, recovering one expended spell slot. You are only able to do this once per day.
-                </Item>
-            </div>
-            <div className='col50'>
-                <StatBlock monsterId={71} />
-            </div>
-        </Page>
-    </Fragment>
+    <section className='intro'>
+        <div className='container'>
+            <h1>Cauldron</h1>
+            <p>Easily build beautiful homebrew content using the 5e SRD</p>
+        </div>
+    </section>
 );
 
 export default Home;
